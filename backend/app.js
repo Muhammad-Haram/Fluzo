fcfdcb866dbbb61346c448bfc19befd525c4939a
Muhammad-Haram <haramh643@gmail.com>
2025-02-17 15:45:45 +0500

    commit

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,8 @@ const connectToDb = require("./db/db.js");
 connectToDb();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
