85890e1e6418fa0f4938bbcb8e642d99ab16b7e5
Muhammad-Haram <haramh643@gmail.com>
2025-02-17 17:07:05 +0500

    register

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const connectToDb = require("./db/db.js");
+const userRoutes = require("./routes/user.routes");
 
 connectToDb();
 
@@ -14,5 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+app.use("/users", userRoutes);
 
 module.exports = app;
--- backend/controller/user.controllers.js
+++ backend/controller/user.controllers.js
@@ -1,13 +1,16 @@
 const userModel = require("../models/user.model");
+const userService = require("../services/user.service");
+const { validationResult } = require("express-validator");
 
 module.exports.registerUser = async (req, res, next) => {
   const errors = validationResult(req);
+
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
 
   const { fullname, email, password } = req.body;
-
+  
   const isUserAlready = await userModel.findOne({ email });
 
   if (isUserAlready) {
