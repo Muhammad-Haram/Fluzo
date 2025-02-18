f3502e4e8f0850f7fae2aad55d29573b0c615c7c
Muhammad-Haram <haramh643@gmail.com>
2025-02-18 11:14:00 +0500

    add doc

--- backend/controller/user.controllers.js
+++ backend/controller/user.controllers.js
@@ -10,7 +10,7 @@ module.exports.registerUser = async (req, res, next) => {
   }
 
   const { fullname, email, password } = req.body;
-  
+
   const isUserAlready = await userModel.findOne({ email });
 
   if (isUserAlready) {
