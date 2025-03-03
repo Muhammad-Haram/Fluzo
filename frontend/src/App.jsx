dc287d6cf6d5aeacfc91c07dd3b9756f73a47a93
Muhammad Haram <haramh643@gmail.com>
2025-03-04 02:07:03 +0500

    routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import UserLogin from './pages/UserLogin'
+import UserSignup from './pages/UserSignup'
+import Captainlogin from './pages/CaptainLogin'
+import CaptainSignup from './pages/CaptainSignup'
 
 const App = () => {
   return (
     <div>
-      <h1 class="underline">
-        Hello world!
-      </h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path='/login' element={<UserLogin />} />
+        <Route path='/signup' element={<UserSignup />} />
+        <Route path='/captain-login' element={<Captainlogin />} />
+        <Route path='/captain-signup' element={<CaptainSignup />} />
+      </Routes>
     </div>
   )
 }
